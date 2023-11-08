71562a751727dc959169237f769c8b64ee096407
shohagroy <pkshohag240@gmail.com>
2023-11-09 01:50:56 +0600

    next js page router initial setup added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { store } from "@/redux/store/store";
+import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ConfigProvider>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </Provider>
+  );
 }
---
+++ src/pages/admin/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, Result } from "antd";
+
+const AdminDashboard = () => {
+  return (
+    <>
+      <main>
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Button type="primary">Back Home</Button>}
+        />
+      </main>
+    </>
+  );
+};
+
+export default AdminDashboard;
